7aeb7ef39516292dce7d8149b6034d798b2557dd
Kareem_Ashraf <Kareem_Ashraf@DESKTOP-720M6HV>
2025-11-11 20:16:05 -0800

    Add project files.

---
+++ ApiGateway.Presintaion/Middelware/AttachSignatureToRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiGateway.Presintaion.Middelware
+{
+    public class AttachSignatureToRequest
+    {
+        private readonly RequestDelegate _next;
+
+        public AttachSignatureToRequest(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Add a custom header
+            context.Request.Headers["Api-Gateway"] = "Signed";
+
+            // Continue the request pipeline
+            await _next(context);
+        }
+    }
+}
---
+++ ApiGateway.Presintaion/Program.cs
@@ -0,0 +1,33 @@
+using ApiGateway.Presintaion.Middelware;
+using EcommerceSharedLibrary.DependencyInjection;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Configuration.AddJsonFile("ocelot.json" ,optional:false ,reloadOnChange:true);
+builder.Services.AddOcelot().AddCacheManager(x => x.WithDictionaryHandle());
+JWTAuthenticationScheme.AddJWtAuthenticationScheme(builder.Services, builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseMiddleware<AttachSignatureToRequest>();
+app.UseOcelot().Wait();
+
+
+
+app.Run();
